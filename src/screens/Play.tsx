06a7e8c5bdc678f97c5e1864ba671c71f8d32789
jose.paulo <jose.paulo@ceuma.br>
2024-08-09 10:24:45 -0300

    ajustes pra testar mais facil

--- src/screens/Play.tsx
+++ src/screens/Play.tsx
@@ -17,12 +17,7 @@ export function Play() {
       <Clock />
       {yourPlayerId && <Controls />}
       <Pixi.In>
-        <Text
-          text="Hello Pixi from HTML!"
-          style={new TextStyle({ fill: [0xffffff, 0x00ff99] })}
-          position={{ x: 0, y: 100 }}
-        />
-        <Container scale={3} position={{ x: 0, y: 300 }}>
+        <Container scale={1} position={{ x: 0, y: 300 }}>
           {playerIds.map((playerId, index) => (
             <Character key={playerId} playerId={playerId} model={index % 2} />
           ))}
