da3ecbb5bca19f17a0304b23ed34a7bad81b14d3
jose.paulo <jose.paulo@ceuma.br>
2024-10-04 10:19:06 -0300

    muda o memo para a função de texture

--- src/components/pixi/BombsLayer.tsx
+++ src/components/pixi/BombsLayer.tsx
@@ -1,44 +1,44 @@
 import { Bomb } from "@/types";
 import { Texture } from "@pixi/core";
 import { AnimatedSprite, Container } from "@pixi/react";
-import React from "react";
+import React, { useMemo } from "react";
 
 // Definindo os tipos das props
 interface BombsLayerProps {
   data: Bomb[];
 }
 
-// Função para carregar as texturas do sprite sheet
+// Função para carregar as texturas do sprite sheet com useMemo
 const getAnimationFrames = (tile: number): Texture[] => {
-  if (tile > 0) {
-    return [Texture.from("bomb_0_a"), Texture.from("bomb_0_b")];
-  }
-  return [Texture.from("bomb_0_a")];
+  return tile > 0 
+    ? [Texture.from("bomb_0_a"), Texture.from("bomb_0_b")] 
+    : [Texture.from("bomb_0_a")];
 };
-
-// Componente BombSprite memoizado para evitar re-renderizações desnecessárias
-const BombSprite: React.FC<{ bomb: Bomb }> = React.memo(({ bomb }) => {
-  const tileSize = 16; // Tamanho do tile
-
-  return (
-    <AnimatedSprite
-      key={bomb.id} // Usando a ID da bomba como chave única
-      position={{ x: bomb.pos.x * tileSize, y: (bomb.pos.y - 1) * tileSize }}
-      textures={getAnimationFrames(1)} // Passa os frames com base no tipo da bomba
-      animationSpeed={0.05} // Velocidade da animação
-      isPlaying={true} // Começa a tocar a animação automaticamente
-      scale={1}
-    />
-  );
-});
 
 // Componente BombsLayer
 export const BombsLayer: React.FC<BombsLayerProps> = ({ data }) => {
+  const tileSize = 16; // Tamanho do tile
+
   return (
     <Container scale={3} position={{ x: 0, y: 0 }}>
-      {data.map((bomb) => (
-        <BombSprite key={bomb.id} bomb={bomb} />
-      ))}
+      {data.map((bomb, index) => {
+        // Memoizando as texturas com base em bomb.type ou outro valor relevante
+        const textures = useMemo(
+          () => getAnimationFrames(1), // Use bomb.type para gerar as texturas dinamicamente
+          [bomb.pos] // Dependência para recalcular as texturas apenas se o bomb.type mudar
+        );
+
+        return (
+          <AnimatedSprite
+            key={bomb.id}
+            position={{ x: bomb.pos.x * tileSize, y: (bomb.pos.y - 1) * tileSize }}
+            textures={textures} // Usa as texturas memoizadas
+            animationSpeed={0.05} // Velocidade da animação
+            isPlaying={true} // Começa a tocar a animação automaticamente
+            scale={1}
+          />
+        );
+      })}
     </Container>
   );
 };
